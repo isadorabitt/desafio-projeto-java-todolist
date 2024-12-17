28edfb439dba84e3dbad5a743fc3d5d1be151d34
Isadora <isa_carvalho_12@hotmail.com>
2024-12-17 12:59:05 -0300

    Primeiro commit - subindo projeto

---
+++ src/main/java/br/com/isadorabittencourt/desafio_java_todolist/DesafioJavaTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.isadorabittencourt.desafio_java_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isadorabittencourt/desafio_java_todolist/DesafioJavaTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isadorabittencourt.desafio_java_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
